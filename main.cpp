25ae98db4a027007eb39ac446df4dee84b67c6e0
Mikhail Mansurov <mansfm@yandex.ru>
2026-05-08 13:05:41 +0000

    functions

--- main.cpp
+++ main.cpp
@@ -1,40 +1,77 @@
 #include <iostream>
 
-//Test
+//сложение:
+int addition(int a, int b)
+{
+    return a + b;
+}
+
+//вычитание:
+int subtraction(int a, int b)
+{
+    return a - b;
+}
+
+//умножение:
+int multiplication(int a, int b)
+{
+    return a * b;
+}
+
+//деление:
+int division(int a, int b)
+{
+    return a / b;
+}
+
+//возведение в степень:
+int power(int a, int b)
+{
+    int c = 1;
+    for (int i = 0; i < b; ++i)
+    {
+        c = c * a;
+    }
+    return c;
+}
+
+//вывод в консоль:
+void printResult(int a, int b, int c, char operation)
+{
+    std::cout << a << ' ' << operation << ' ' << b << " = " << c << '\n';
+}
+
+
 int main()
 {
-	int a = 2;
-	int b = 2;
-	int c = 0;
+    int a = 2;
+    int b = 2;
+    int c = 0;
 
-	// сложение:
-	c = a + b;
-	std::cout << a << " + " << b << " = " << c << '\n';
+    // сложение:
+    c = addition(a, b);
+    printResult(a, b, c, '+');
 
-	// вычитание:
-	c = a - b;
-	std::cout << a << " - " << b << " = " << c << '\n';
+    // вычитание:
+    c = subtraction(a, b);
+    printResult(a, b, c, '-');
 
-	//умножение:
-	c = a * b;
-	std::cout << a << " * " << b << " = " << c << '\n';
+    //умножение:
+    c = multiplication(a, b);
+    printResult(a, b, c, '*');
 
-	// деление:
-	if (b != 0)
-	{
-		c = a / b;
-		std::cout << a << " / " << b << " = " << c << '\n';
-	}
-	else
-	{
-		std::cout << "Error! Division by zero\n";
-	}
+    // деление:
+    if (b != 0)
+    {
+        c = division(a, b);
+        printResult(a, b, c, '/');
+    }
+    else
+    {
+        std::cout << "Error! Division by zero\n";
+    }
 
-	// возведение в степень:
-	c = 1;
-	for (int i = 0; i < b; ++i)
-	{
-		c = c * a;
-	}
-	std::cout << a << " ^ " << b << " = " << c << '\n';
+    // возведение в степень:
+    c = power(a, b);
+    printResult(a, b, c, '^');
 }
